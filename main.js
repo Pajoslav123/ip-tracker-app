ad28e26aa8f40e0f43db1820bf69d67c08806158
Pajoslav123 <47066140+Pajoslav123@users.noreply.github.com>
2021-11-14 18:09:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,26 @@
-var ipData;
-var ipAddress
-var isp
-var country
-var city
-var lat
-var long
-var timezone
-var map;
-function saveIpData(data){
-
-    ipAddress = data.ip;
-    isp = data.isp;
-    city = data.location.city;
-    country = data.location.country;
-    lat = data.location.lat;
-    long = data.location.lng;
-    timezone = data.location.timezone;
-
-    document.getElementById("isp").innerHTML = isp;
-    document.getElementById("location").innerHTML = country+","+city;
-    document.getElementById("timezone").innerHTML = timezone;
-    document.getElementById("ip").innerHTML=ipAddress;
-    console.log(isp + "THIS IS THE ISP");
-    console.log(long)
-
-}
+var ipData,ipAddress,isp,country,city,lat,long,timezone,map;
 function generateIpDetails(){
     var ip = document.getElementById("ipTxt").value;
     var api_key = "at_DWbeNhrqKKpRNwGFekYocieSqi2mP";
-    const http = new XMLHttpRequest();
-    http.open("GET","https://geo.ipify.org/api/v2/country,city?apiKey="+api_key+"&ipAddress="+ip, true);
-    http.send();
-    http.onload = function(e) {
-        try {
-            ipData = JSON.parse(http.responseText)
-            saveIpData(ipData);
-            generateMap(lat,long);
-            console.log(ipData);
-        } catch(err) {
-            console.log(err);
-        }
-
-    }
-    
+    fetch("https://geo.ipify.org/api/v2/country,city?apiKey="+api_key+"&ipAddress="+ip)
+    .then(data =>{
+        return data.json();
+    })
+    .then(data => {
+        ipAddress = data.ip;
+        isp = data.isp;
+        city = data.location.city;
+        country = data.location.country;
+        lat = data.location.lat;
+        long = data.location.lng;
+        timezone = data.location.timezone;
+        
+        document.getElementById("isp").innerHTML = isp;
+        document.getElementById("location").innerHTML = country+","+city;
+        document.getElementById("timezone").innerHTML = timezone;
+        document.getElementById("ip").innerHTML=ipAddress;
+        generateMap(lat,long);
+        });
     // ontimeout, onabort. onerror...
 }
 function generateMap(lat,long){
